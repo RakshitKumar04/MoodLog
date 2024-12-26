7f34a970e0933e0d867e827c0dbf79a074331ab2
Rakshit Sharma <kumarrakshit0402@gmail.com>
2024-12-26 17:33:52 +0530

    Pushing the project to Github

---
+++ src/main/java/me/devrax/moodLog/moodLog.java
@@ -0,0 +1,13 @@
+package me.devrax.moodLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class moodLog {
+
+	public static void main(String[] args) {
+		SpringApplication.run(moodLog.class, args);
+	}
+
+}
---
+++ src/test/java/me/devrax/moodLog/MoodLogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devrax.moodLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodLogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
