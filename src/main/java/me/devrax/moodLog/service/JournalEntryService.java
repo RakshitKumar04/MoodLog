9a7db0099ea665cb519594a22ba7d43b8fac403d
Rakshit Sharma <kumarrakshit0402@gmail.com>
2024-12-30 21:26:14 +0530

    Trying to establish delete api

--- src/main/java/me/devrax/moodLog/service/JournalEntryService.java
+++ src/main/java/me/devrax/moodLog/service/JournalEntryService.java
@@ -28,6 +28,10 @@ public class JournalEntryService {
         userService.saveEntry(user);
     }
 
+    public void saveEntry(JournalEntry journalEntry){
+        JournalEntryRepository.save(journalEntry);
+    }
+
     public List<JournalEntry> getAll(){
         return journalEntryRepository.findAll();
     }
@@ -36,7 +40,10 @@ public class JournalEntryService {
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id){
+    public void deleteById(ObjectId id, String userName){
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
         journalEntryRepository.deleteById(id);
     }
 }
