c34da5fa64291d2a05e156bf3f00bc904d874e7d
Rakshit Sharma <kumarrakshit0402@gmail.com>
2024-12-31 14:30:29 +0530

    Done with transactional annotation

--- src/main/java/me/devrax/moodLog/service/JournalEntryService.java
+++ src/main/java/me/devrax/moodLog/service/JournalEntryService.java
@@ -6,7 +6,9 @@ import me.devrax.moodLog.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -20,16 +22,23 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            user.setUserName(null);
+            userService.saveEntry(user);
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException("An Error occurred while saving the entry.", e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry){
-        JournalEntryRepository.save(journalEntry);
+        journalEntryRepository.save(journalEntry);
     }
 
     public List<JournalEntry> getAll(){
